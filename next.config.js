a93c96a7f02f2c2fe6f053fa1ed9b8239e4ad56d
Haykal M. Zaidi <hgm11105557@gmail.com>
2026-06-17 13:05:38 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
   trailingSlash: false,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: false,
+  },
   env: {
     NEXT_PUBLIC_SITE_URL: process.env.NEXT_PUBLIC_SITE_URL || 'https://opentuwa.com',
   },
