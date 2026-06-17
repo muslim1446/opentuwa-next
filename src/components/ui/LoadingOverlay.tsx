685cddcf149a1e82125d66b4f9f011aca08ccb9c
Haykal M. Zaidi <hgm11105557@gmail.com>
2026-06-17 15:48:07 +0800

    Update LoadingOverlay.tsx

--- src/components/ui/LoadingOverlay.tsx
+++ src/components/ui/LoadingOverlay.tsx
@@ -11,7 +11,6 @@ export function LoadingOverlay({ onStart }: LoadingOverlayProps) {
         <h2 className="heroz-title-text" id="doors-hero-title" />
       </nav>
       <div className="loader-content">
-        <div className="loader-spinner" />
         <div className="loader-text" id="loader-text" />
         <button id="start-btn" className="start-btn" data-i18n="player.start" onClick={onStart}>
           Start
